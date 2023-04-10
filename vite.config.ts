6d7983340310ccd377147f2b77094cb1801fd3bb
MagicCrazyMan <350088648@qq.com>
2023-04-10 13:44:14 +0800

    Initialize template

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import vue from "@vitejs/plugin-vue";
+import eslint from "vite-plugin-eslint";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    eslint(),
+    vue({
+      template: { transformAssetUrls },
+    }),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+      styles: {
+        configFile: "src/styles/settings.scss",
+      },
+    }),
+  ],
+  define: { "process.env": {} },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
+  },
+  server: {
+    port: 3000,
+  },
+});
